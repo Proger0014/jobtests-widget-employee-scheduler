e56372e06d7a5c569ab369f7bbcd260a52a851a5
proger0014 <iakov56@yandex.ru>
2025-01-13 20:32:59 +0600

    update gitignore, add bootstrap for testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(-1);
+
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_DEBUG', true);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+Yii::setAlias("@proger0014/tests/yii2", __DIR__);
+Yii::setAlias('@proger0014/yii2', dirname(__DIR__) . '/src');
