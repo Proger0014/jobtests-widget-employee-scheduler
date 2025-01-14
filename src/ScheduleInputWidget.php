91b8dd782c381c7776efca7470cc1f431fdd5337
proger0014 <iakov56@yandex.ru>
2025-01-14 22:35:06 +0600

    start view design

--- src/ScheduleInputWidget.php
+++ src/ScheduleInputWidget.php
@@ -6,6 +6,8 @@ use proger0014\yii2\model\ScheduleModel;
 use yii\base\InvalidArgumentException;
 use yii\base\Model;
 use yii\bootstrap\InputWidget;
+use yii\helpers\Html;
+use yii\helpers\StringHelper;
 
 class ScheduleInputWidget extends InputWidget
 {
@@ -22,6 +24,49 @@ class ScheduleInputWidget extends InputWidget
             $this->checkRequirements();
             $this->configureAttributes();
         }
+    }
+
+    public function run(): string {
+        $render = Html::beginTag('div', ['class' => 'widget panel panel-default']);
+        $render .= Html::beginTag('div', ['class' => 'panel-body']);
+
+        $render .= Html::tag('p', 'Рабочие часы', ['class' => 'title-1']);
+        $render .= Html::tag('p', 'Установить рабочие часы');
+        $render .= Html::tag('hr', null, ['class' => 'mb-25']);
+
+        $render .= $this->renderSwitchSection('Учитывать часовой пояс', 'icon', $this->enableTimeZone);
+        $render .= $this->renderSwitchSection('Производственный календарь', 'icon', $this->enableProductionCalendar);
+
+        $render .= Html::endTag('div');
+        return $render . Html::endTag('div');
+    }
+
+    // TODO name of input
+    private function renderSwitchSection(string $label, $icon, bool $enabled): string {
+        $render = Html::beginTag('div', $this->getWrapperOptions(['class' => 'd-flex jc-sb'], $enabled));
+
+        $render .= Html::beginTag('div');
+        $render .= Html::tag('label', $label);
+        // TODO icon
+        $render .= Html::tag('span', $icon);
+        $render .= Html::endTag('div');
+
+        $render .= $this->renderSwitch('switch');
+
+        return $render . Html::endTag('div');
+    }
+
+    // TODO name
+    private function renderSwitch(string $name): string {
+        return Html::checkbox($name);
+    }
+
+    private function getWrapperOptions(array $src, bool $enabled): array {
+        $enabledClass = $enabled ? '' : 'disabled';
+
+        Html::addCssClass($src, $enabledClass);
+
+        return $src;
     }
 
     private function getModel(): ScheduleModel {
