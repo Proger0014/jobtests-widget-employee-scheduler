3e858f237c381b1a95727ba2d1278936c99b391b
proger0014 <iakov56@yandex.ru>
2025-01-14 07:11:22 +0600

    add fonts dependency, add css to asset bundle

--- src/WidgetAsset.php
+++ src/WidgetAsset.php
@@ -10,6 +10,11 @@ class WidgetAsset extends AssetBundle
 {
     public $sourcePath = "@proger0014/assets";
 
+    public $css = [
+        '@proger0014/assets/proger0014.widget.css',
+        '@npm/fontsource--roboto/400.css'
+    ];
+
     public $depends = [
         BootstrapAsset::class,
         BootstrapPluginAsset::class,
