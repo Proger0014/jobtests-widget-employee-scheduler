039629db2177a5f0c2e5d270614730853b2daa91
proger0014 <iakov56@yandex.ru>
2025-01-14 20:31:05 +0600

    fix

--- src/ScheduleInputWidget.php
+++ src/ScheduleInputWidget.php
@@ -29,7 +29,7 @@ class ScheduleInputWidget extends InputWidget
     public function checkRequirements(): void {
         $model = $this->model[$this->attribute];
 
-        if (!$this->model instanceof ScheduleModel
+        if (!model instanceof ScheduleModel
             || !is_subclass_of($model, ScheduleModel::class, false)) {
             throw new InvalidArgumentException("model in attribute[$this->attribute] must be of ScheduleModel or extends from that");
         }
