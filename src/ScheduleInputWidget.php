b12cdb7360ab3ba94a86ea90efde025f4c43a2b6
Iakov Kostenyuk <iakov.kostenyuk@gmail.com>
2025-01-16 15:31:39 +0600

    update widget

--- src/ScheduleInputWidget.php
+++ src/ScheduleInputWidget.php
@@ -4,6 +4,7 @@ namespace proger0014\yii2;
 
 use proger0014\yii2\model\ScheduleModel;
 use yii\base\InvalidArgumentException;
+use yii\bootstrap\Button;
 use yii\bootstrap\InputWidget;
 use yii\helpers\Html;
 
@@ -19,9 +20,19 @@ class ScheduleInputWidget extends InputWidget
         parent::init();
 
         if ($this->model) {
-            $this->checkRequirements();
+            $this->checkRequirementsModel();
             $this->configureAttributes();
+        } else {
+            $this->checkRequirements();
         }
+    }
+
+    public function getBaseName(): string {
+        if (!$this->model) {
+            return $this->name;
+        }
+
+        return $this->model->formName() . "[$this->attribute]";
     }
 
     public function run(): string {
@@ -35,13 +46,81 @@ class ScheduleInputWidget extends InputWidget
         $render .= $this->renderSwitchSection('Учитывать часовой пояс', 'icon', $this->enableTimeZone);
         $render .= $this->renderSwitchSection('Производственный календарь', 'icon', $this->enableProductionCalendar);
 
+        $render .= $this->renderTimeSection();
+
         $render .= Html::endTag('div');
         return $render . Html::endTag('div');
     }
 
-    // TODO name of input
+    private function renderTimeSection(): string {
+        $render = Html::beginTag('div', ['class' => 'time-list mb-25']);
+
+        $render .= Html::beginTag('div', ['class' => 'item panel panel-default border']);
+        $render .= Html::beginTag('div', ['class' => 'panel-body']);
+        $render .= Html::beginTag('div', ['class' => 'd-flex jc-sb ai-center']);
+
+        $render .= Html::beginTag('div', ['class' => 'd-flex']);
+        $render .= $this->renderDayInput('Пн', 0);
+        $render .= $this->renderDayInput('Вт', 1);
+        $render .= $this->renderDayInput('Ср', 2);
+        $render .= $this->renderDayInput('Чт', 3);
+        $render .= $this->renderDayInput('Пт', 4);
+        $render .= $this->renderDayInput('Сб', 5);
+        $render .= $this->renderDayInput('Вс', 6);
+        $render .= Html::endTag('div');
+
+        $render .= Html::beginTag('div', ['class' => 'd-flex jc-sb ai-center']);
+
+        $render .= Html::beginTag('div', ['class' => 'd-flex jc-sb']);
+        $render .= $this->renderTimeInput();
+        $render .= Html::beginTag('div', ['class' => 'd-flex jc-center ai-center mx-5']);
+        $render .= Html::tag('hr', null, ['class' => 'straight']);
+        $render .= Html::endTag('div');
+        $render .= $this->renderTimeInput();
+        $render .= Html::endTag('div');
+
+        $render .= Html::beginTag('div', ['class' => 'd-flex jc-sb']);
+        $render .= $this->renderIconButton('1');
+        $render .= $this->renderIconButton('2', false);
+        $render .= Html::endTag('div');
+
+        $render .= Html::endTag('div');
+
+
+        $render .= Html::endTag('div');
+        $render .= Html::endTag('div');
+        $render .= Html::endTag('div');
+
+        return $render . Html::endTag('div');
+    }
+
+    private function renderIconButton($icon, bool $withMr = true): string {
+        $class = 'btn btn-icon';
+
+        if ($withMr) {
+            $class .= ' mr-8';
+        }
+
+        return Html::tag('button', $icon, ['class' => $class]);
+    }
+
+    private function renderTimeInput(): string {
+        $render = Html::beginTag('div', ['class' => 'form-group']);
+        $render .= Html::input('time', null, '00:00', ['min' => '00:00', 'max' => '23:59']);
+        return $render . Html::endTag('div');
+    }
+
+    private function renderDayInput(string $content, int $index): string {
+        $render = Html::beginTag('label', ['class' => 'day']);
+
+        $render .= Html::checkbox($this->getBaseName() . "[work_time][$index][day]");
+        $render .= Html::tag('span', $content);
+
+        return $render . Html::endTag('label');
+    }
+
     private function renderSwitchSection(string $label, $icon, bool $enabled): string {
-        $render = Html::beginTag('div', $this->getWrapperOptions(['class' => 'd-flex jc-sb'], $enabled));
+        $render = Html::beginTag('div', ['class' => 'd-flex jc-sb']);
 
         $render .= Html::beginTag('div');
         $render .= Html::tag('label', $label);
@@ -49,22 +128,17 @@ class ScheduleInputWidget extends InputWidget
         $render .= Html::tag('span', $icon);
         $render .= Html::endTag('div');
 
-        $render .= $this->renderSwitch('switch');
+        if ($enabled) {
+            $render .= $this->renderSwitch();
+        }
 
         return $render . Html::endTag('div');
     }
 
-    // TODO name
-    private function renderSwitch(string $name): string {
-        return Html::checkbox($name);
-    }
-
-    private function getWrapperOptions(array $src, bool $enabled): array {
-        $enabledClass = $enabled ? '' : 'disabled';
-
-        Html::addCssClass($src, $enabledClass);
-
-        return $src;
+    private function renderSwitch(): string {
+        return Html::checkbox('', false, [
+            'form' => ''
+        ]);
     }
 
     private function getModel(): ScheduleModel {
@@ -74,12 +148,21 @@ class ScheduleInputWidget extends InputWidget
     /**
      * @throws \InvalidArgumentException
      */
-    public function checkRequirements(): void {
+    public function checkRequirementsModel(): void {
         $model = $this->model[$this->attribute];
 
         if (!$model instanceof ScheduleModel
             || !is_subclass_of($model, ScheduleModel::class, false)) {
             throw new InvalidArgumentException("model in attribute[$this->attribute] must be of ScheduleModel or extends from that");
+        }
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public function checkRequirements(): void {
+        if (!$this->name) {
+            throw new InvalidArgumentException("name is required");
         }
     }
 
